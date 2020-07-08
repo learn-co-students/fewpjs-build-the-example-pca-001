47470f2ed9468e13b9478736048faa6e403014b4
Pat McClernan <patmccler@gmail.com>
2020-07-08 09:00:41 -0400

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,53 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
 
+// setup listeners
+(function () {
+  likes = document.querySelectorAll(".like")
+  console.log(likes)
+  likes.forEach( like => like.onclick = likeClicked)
 
+})()
+
+
+
+
+
+
+
+function likeClicked(e) {
+  targetElem = e.currentTarget
+
+  mimicServerCall()
+  .then(resp => {
+    toggleLike(targetElem)})
+  .catch(error => { displayError(error)})
+}
+
+function toggleLike(likeLi) {
+  span = likeLi.firstElementChild
+  // toggle returns true if added, false if removed
+  span.innerText = (span.classList.toggle("activated-heart") ? FULL_HEART : EMPTY_HEART)
+
+}
+
+function displayError(error) {
+  errorModal = document.getElementById("modal")
+  errorText = document.getElementById("modal-message")
+
+  errorModal.classList.remove("hidden")
+  errorText.innerText = error
+
+  function hideError() {
+    errorText.innerText = ""
+    errorModal.classList.add("hidden")
+  }
+
+  setTimeout(hideError, 5000)
+}
 
 
 //------------------------------------------------------------------------------
