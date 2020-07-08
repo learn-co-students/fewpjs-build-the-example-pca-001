312f48aa5e4bd0797ffb81654e87c7ca5adad06a
Pat McClernan <patmccler@gmail.com>
2020-07-08 11:05:32 -0400

    Done.

--- main.js
+++ main.js
@@ -2,36 +2,23 @@
 const EMPTY_HEART = '♡';
 const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
-
 // setup listeners
 (function () {
-  likes = document.querySelectorAll(".like")
-  console.log(likes)
-  likes.forEach( like => like.onclick = likeClicked)
-
+  document.querySelectorAll(".like").forEach( like => like.onclick = likeClicked)
 })()
-
-
-
-
-
-
 
 function likeClicked(e) {
   targetElem = e.currentTarget
 
   mimicServerCall()
-  .then(resp => {
-    toggleLike(targetElem)})
-  .catch(error => { displayError(error)})
+  .then(resp => toggleLike(targetElem))
+  .catch(error => displayError(error))
 }
 
 function toggleLike(likeLi) {
   span = likeLi.firstElementChild
   // toggle returns true if added, false if removed
   span.innerText = (span.classList.toggle("activated-heart") ? FULL_HEART : EMPTY_HEART)
-
 }
 
 function displayError(error) {
