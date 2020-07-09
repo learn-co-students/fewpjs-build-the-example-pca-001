d17bff3744aac60987d2bbbde5e2fc44f645b2c9
Jo Fischer <josephinefischer9@gmail.com>
2020-07-09 12:05:36 -0400

    done

--- main.js
+++ main.js
@@ -3,9 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeButtons = document.querySelectorAll(".like-glyph")
 
+function likeHeart(event) {
+  let heart = event.target
+  console.log(heart)
+  mimicServerCall("URL")
+  .then(function(response) {
 
+    heart.innerText = (heart.classList.toggle("activated-heart") ? FULL_HEART : EMPTY_HEART)
+    // if (heart.innerText === EMPTY_HEART) {
+    //   heart.innerText = FULL_HEART
+    // }
+    // else {
+    //   heart.innerText = EMPTY_HEART
+    // }
+  })
+  .catch(function(error) {
+    console.log(error.message)
+  })
+}
 
+for(const button of likeButtons) {
+  button.addEventListener("click", likeHeart)
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
